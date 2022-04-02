34c7d95036730a62777b64f610112d6fd18c3d38
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-04-02 14:33:51 -0300

    Completed function that stores user input into an object and then stores that object into an array

--- library.js
+++ library.js
@@ -11,7 +11,11 @@ function Book(title, author, pages, hasBeenRead) {
 
 function addBookToLibrary() {
 	
-	
+	let newBook = new Book(title, author, pages, hasBeenRead);
+	myLibrary.push(newBook);
+	console.log(newBook.title);
+	console.log(myLibrary);
+	console.log(myLibrary[0].title);
 }
 
 // Test values
@@ -20,5 +24,12 @@ let author = window.prompt("title: ");
 let pages = window.prompt("title: ");
 let hasBeenRead = window.prompt("title: ");
 
-let newBook = new Book(title, author, pages, hasBeenRead);
-console.log(newBook.title);
+addBookToLibrary();
+
+title = window.prompt("title: ");
+author = window.prompt("title: ");
+
+addBookToLibrary();
+console.log(myLibrary[1].title);
+
+
