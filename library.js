b245afac7a56deb4a1623c0bac878c07435bc557
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-04-02 15:16:39 -0300

    Moving locations and machines, working on rendering book cards

--- library.js
+++ library.js
@@ -9,13 +9,12 @@ function Book(title, author, pages, hasBeenRead) {
 	this.hasBeenRead = hasBeenRead;
 }
 
+// Create book object and store it into an array
 function addBookToLibrary() {
 	
 	let newBook = new Book(title, author, pages, hasBeenRead);
 	myLibrary.push(newBook);
-	console.log(newBook.title);
-	console.log(myLibrary);
-	console.log(myLibrary[0].title);
+	render();
 }
 
 // Test values
@@ -24,12 +23,11 @@ let author = window.prompt("title: ");
 let pages = window.prompt("title: ");
 let hasBeenRead = window.prompt("title: ");
 
-addBookToLibrary();
+function render() {
+	
+	
+}
 
-title = window.prompt("title: ");
-author = window.prompt("title: ");
 
-addBookToLibrary();
-console.log(myLibrary[1].title);
 
 
