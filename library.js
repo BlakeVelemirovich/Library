bd621f335351144228477372cf170ee91268afa8
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-04-02 13:00:40 -0300

    Adding test variables and logs

--- library.js
+++ library.js
@@ -1,9 +1,24 @@
 let myLibrary = [];
 
-function Book() {
+// Book constructor
+function Book(title, author, pages, hasBeenRead) {
 	
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.hasBeenRead = hasBeenRead;
 }
 
 function addBookToLibrary() {
 	
+	
 }
+
+// Test values
+let title = window.prompt("title: ");
+let author = window.prompt("title: ");
+let pages = window.prompt("title: ");
+let hasBeenRead = window.prompt("title: ");
+
+let newBook = new Book(title, author, pages, hasBeenRead);
+console.log(newBook.title);
